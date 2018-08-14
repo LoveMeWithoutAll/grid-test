a8ed8b03b9335d1a98c5c3758e70bb2106cf16ae
YoungSeon.Ahn <ys.ahn@outlook.com>
2018-08-14 13:56:58 +0900

    trival layout fix

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,14 +4,14 @@
       <v-layout column align-center>
         <h1 class="display-3 mb-2">Grid test</h1>
         <h2 class="title mb-2">concentration training</h2>
-        <blockquote class="mb-4">First, find 0 . Then, next.</blockquote>
+        <blockquote class="mb-2">First, find 0 . Then, next.</blockquote>
         <timer></timer>
         <div v-if="!clear && started" class="text-xs-center">
           <h3>Next</h3>
           <h3 class="display-2">{{ nextNum }}</h3>
         </div>
         <timer-button v-if="!started"></timer-button>
-        <v-spacer class="mb-4"></v-spacer>
+        <v-spacer class="mb-3"></v-spacer>
         <grid></grid>
         <v-spacer class="mb-4"></v-spacer>
         <div v-if="clear">
