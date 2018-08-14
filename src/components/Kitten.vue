e2dffc786e3f006234484211da752bd5c236f50e
YoungSeon.Ahn <ys.ahn@outlook.com>
2018-08-14 12:39:15 +0900

    Clear event: show cat image

---
+++ src/components/Kitten.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="well kittens">
+    <img :src="catimgsrc" />
+  </div>
+</template>
+
+<style scoped>
+</style>
+
+<script>
+import { mapFields } from 'vuex-map-fields'
+
+export default {
+  computed: {
+    ...mapFields(['timestamp']),
+    catimgsrc () {
+      return 'http://thecatapi.com/api/images/get?size=med&ts=' + this.timestamp
+    }
+  }
+}
+</script>
