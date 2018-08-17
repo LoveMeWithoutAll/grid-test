348435fe7ed398492c0e62390d893cbbf9eb0e9a
YoungSeon.Ahn <ys.ahn@outlook.com>
2018-08-17 16:00:58 +0900

    Init firebase vuefire

--- src/main.js
+++ src/main.js
@@ -4,10 +4,21 @@ import './plugins/vuetify'
 import App from './App.vue'
 import router from './router'
 import store from '@/vuex/store'
-
+import VueFire from 'vuefire'
+import firebase from 'firebase'
+import { FIREBASE_CONFIG } from '@/config'
 import VueGitHubCorners from 'vue-gh-corners'
 import 'vue-gh-corners/dist/vue-github-corners.css'
+
 Vue.use(VueGitHubCorners)
+Vue.use(VueFire)
+
+const firebaseApp = firebase.initializeApp(FIREBASE_CONFIG)
+const db = firebaseApp.database()
+
+console.log(db.ref('scores').on('value', (snapshot) => {
+  console.log(snapshot.val())
+}))
 
 Vue.config.productionTip = false
 
