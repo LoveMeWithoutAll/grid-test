4ad8d0bc1e0bd217f1e98a54db2e5612e945fa15
YoungSeon.Ahn <ys.ahn@outlook.com>
2018-08-22 10:02:53 +0900

    Add START GAME button

--- src/App.vue
+++ src/App.vue
@@ -7,8 +7,11 @@
     <v-footer app>
       <v-layout align-center justify-space-between>
         <span>&copy; 2018. YoungSeon Ahn</span>
-        <router-link to="/ranking">
-          <v-btn color="gray" outline right flat small>SHOW RANKING</v-btn>
+        <router-link to="/" v-if="!showRanking">
+          <v-btn color="gray" @click="changeBtn" outline right flat small>START GAME</v-btn>
+        </router-link>
+        <router-link to="/ranking" v-if="showRanking">
+          <v-btn color="gray" @click="changeBtn" outline right flat small>SHOW RANKING</v-btn>
         </router-link>
       </v-layout>
     </v-footer>
@@ -17,11 +20,32 @@
 
 <script>
 import GithubCorners from '@/views/GithubCorners'
+import { mapMutations } from 'vuex'
+import * as types from '@/vuex/mutation_types'
 
 export default {
   name: 'App',
   components: {
     GithubCorners
+  },
+  data () {
+    return {
+      showRanking: true
+    }
+  },
+  created () {
+    this.showRanking = document.location.hash.split('#/')[1] === ''
+  },
+  methods: {
+    changeBtn () {
+      if (!this.showRanking) this.restart()
+      this.showRanking = !this.showRanking
+    },
+    ...mapMutations({start: types.START, clear: types.CLEAR}),
+    restart () {
+      this.clear()
+      this.start()
+    }
   }
 }
 </script>
