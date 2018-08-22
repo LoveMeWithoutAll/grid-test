f45464b275941f2121ce48aa66b5cf4a3f4a98d0
YoungSeon.Ahn <ys.ahn@outlook.com>
2018-08-22 10:31:42 +0900

    Seperate Footer button

--- src/App.vue
+++ src/App.vue
@@ -7,12 +7,7 @@
     <v-footer app>
       <v-layout align-center justify-space-between>
         <span>&copy; 2018. YoungSeon Ahn</span>
-        <router-link to="/" v-if="!showRanking">
-          <v-btn color="gray" @click="changeBtn" outline right flat small>START GAME</v-btn>
-        </router-link>
-        <router-link to="/ranking" v-if="showRanking">
-          <v-btn color="gray" @click="changeBtn" outline right flat small>SHOW RANKING</v-btn>
-        </router-link>
+        <footer-button></footer-button>
       </v-layout>
     </v-footer>
   </v-app>
@@ -20,32 +15,13 @@
 
 <script>
 import GithubCorners from '@/views/GithubCorners'
-import { mapMutations } from 'vuex'
-import * as types from '@/vuex/mutation_types'
+import FooterButton from '@/components/FooterButton'
 
 export default {
   name: 'App',
   components: {
-    GithubCorners
-  },
-  data () {
-    return {
-      showRanking: true
-    }
-  },
-  created () {
-    this.showRanking = document.location.hash.split('#/')[1] === ''
-  },
-  methods: {
-    changeBtn () {
-      if (!this.showRanking) this.restart()
-      this.showRanking = !this.showRanking
-    },
-    ...mapMutations({start: types.START, clear: types.CLEAR}),
-    restart () {
-      this.clear()
-      this.start()
-    }
+    GithubCorners,
+    FooterButton
   }
 }
 </script>
