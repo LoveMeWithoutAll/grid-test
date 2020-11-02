15de0bcd4f93c6b29ce21c546afdae797599a379
YoungSeon.Ahn <ys.ahn@outlook.com>
2020-11-02 15:10:36 +0900

    trival fix

--- src/services/index.js
+++ src/services/index.js
@@ -1,1 +1,4 @@
-export const shuffleArray = (inputNumber) => Array.from(Array(inputNumber * inputNumber).keys()).sort(() => 0.5 - Math.random()).map((v) => v + 1)
+export const shuffleArray = inputNumber =>
+  Array.from(Array(inputNumber * inputNumber).keys())
+    .sort(() => 0.5 - Math.random())
+    .map(v => v + 1);
